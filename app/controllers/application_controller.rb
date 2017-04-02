0f2e950e1983c8f05878ebd03dfeaae7d19e8599
adamewhite <evilofbanality@gmail.com>
2017-04-02 11:23:13 -0400

    remove logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
+  # private
 
-  def confirm_logged_in
-    unless session[:user_id]
-      flash[:notice] = "Please log in."
-      redirect_to(access_login_path)       # redirect_to prevents requested action from running
-    end
-  end
+  # def confirm_logged_in
+  #   unless session[:user_id]
+  #     flash[:notice] = "Please log in."
+  #     redirect_to(access_login_path)       # redirect_to prevents requested action from running
+  #   end
+  # end
 
 end
