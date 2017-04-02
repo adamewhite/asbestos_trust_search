5be92411ec3c4a76eaab190146af15934e1835e1
adamewhite <evilofbanality@gmail.com>
2017-04-02 13:09:50 -0400

    include seed functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+trust_csv_read = File.read(Rails.root.join('lib', 'seeds', 'seed_trust_data.csv'))
+trust_csv = CSV.parse(trust_csv_read, :headers => true, :encoding => 'ISO-8859-1')
+trust_csv.each do |row|
+	Trust.create!(row.to_hash)
+end
+
+site_csv_read = File.read(Rails.root.join('lib', 'seeds', 'seed_site_data.csv'))
+site_csv = CSV.parse(site_csv_read, :headers => true, :encoding => 'ISO-8859-1')
+site_csv.each do |row|
+	Site.create!(row.to_hash)
+end
