dc3286fe0240f83763aace546aa94e83a45a6a73
adamewhite <evilofbanality@gmail.com>
2017-04-09 14:17:00 -0400

    change bottom padding

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,11 +3,11 @@ class Site < ActiveRecord::Base
 	before_create :set_dates
 	belongs_to :trust
 
-	scope :keyword_search, lambda {|query| where(["name ILIKE :query OR city ILIKE :query OR state ILIKE :query OR country ILIKE :query", query: "%#{query}%"])}
-	scope :name_search, lambda {|name| where(["name ILIKE ?", "%#{name}%"]) if name.present?}
-	scope :city_search, lambda {|city| where(["city ILIKE ?", "%#{city}%"]) if city.present?}
-	scope :state_search, lambda {|state| where(["state ILIKE ?", "%#{state}%"]) if state.present?}
-	scope :country_search, lambda {|country| where(["country ILIKE ?", "%#{country}%"]) if country.present?}
+	scope :keyword_search, lambda {|query| where(["name LIKE :query OR city LIKE :query OR state LIKE :query OR country LIKE :query", query: "%#{query}%"])}
+	scope :name_search, lambda {|name| where(["name LIKE ?", "%#{name}%"]) if name.present?}
+	scope :city_search, lambda {|city| where(["city LIKE ?", "%#{city}%"]) if city.present?}
+	scope :state_search, lambda {|state| where(["state LIKE ?", "%#{state}%"]) if state.present?}
+	scope :country_search, lambda {|country| where(["country LIKE ?", "%#{country}%"]) if country.present?}
 	scope :date_search, lambda {|date_from, date_to| where.not(["? > end_date OR ? < start_date", date_from, date_to])}
 
   def set_dates
